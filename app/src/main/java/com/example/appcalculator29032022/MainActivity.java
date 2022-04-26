c00b3080ea123600b4f9efa2a1743cea691f3e85
Phat <phatdroid94@gmail.com>
2022-04-26 18:19:16 +0700

    division

--- app/src/main/java/com/example/appcalculator29032022/MainActivity.java
+++ app/src/main/java/com/example/appcalculator29032022/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     // Global scope
@@ -54,6 +56,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        //Case cho phép chia
+        // 1 : Kiểm tra input
+        //      a : Nếu value của input 2 bằng không thì xuất ra lỗi
+        //      b : Nếu khác 0 thì xử lý theo phép chia trả về số thực
+        //2 : Làm tròn : Nếu chia phần thập phân lớn thì chỉ làm tròn 2 số
+
+        mBtnDivision.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Get input form edittext
+                String textNumber1 = mEdtNumber1.getText().toString();
+                String textNumber2 = mEdtNumber2.getText().toString();
+
+                // Empty input
+                if (textNumber1.isEmpty() || textNumber2.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Input 1 or 2 is empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                // Convert type
+                float number1 = Float.parseFloat(textNumber1);
+                float number2 = Float.parseFloat(textNumber2);
+
+                // Check input number 2 != 0
+                if (number2 <= 0){
+                    Toast.makeText(MainActivity.this, "Can not divide by 0", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                float result = number1 / number2;
+
+                DecimalFormat f = new DecimalFormat("##.00");
+                mTvResult.setText("Result = " + f.format(result));
+            }
+        });
     }
 
 }
